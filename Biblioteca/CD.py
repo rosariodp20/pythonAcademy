970de3fdba2658c47d00dc9a99a7d51e8347872e
rosariodp20 <33570186+rosariodp20@users.noreply.github.com>
2021-03-03 17:24:53 +0100

    Delete Biblioteca directory

--- Biblioteca/CD.py
+++
@@ -1,7 +0,0 @@
-from Articolo import Articolo
-
-class CD(Articolo):
-
-    def __init__(self, collocazione:str,  autore:str, titolo:str, genere:str, tipo:str):
-        self.genere=genere
-        super().__init__(collocazione, titolo, autore, tipo)
--- Biblioteca/Libro.py
+++
@@ -1,7 +0,0 @@
-from Articolo import Articolo
-
-class Libro(Articolo):
-
-    def __init__(self, collocazione:str,  autore:str, titolo:str, genere:str, tipo:str):
-        self.genere=genere
-        super().__init__(collocazione, titolo, autore, tipo)
